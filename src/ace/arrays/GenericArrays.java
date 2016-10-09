08816111b781c6baa974fd77563522cbe4e98777
Javier Santo Domingo <j-a-s-d@coderesearchlabs.com>
2016-10-09 15:16:35 -0300

    improving GenericArrays

--- src/ace/arrays/GenericArrays.java
+++ src/ace/arrays/GenericArrays.java
@@ -3,61 +3,119 @@
 package ace.arrays;
 
 import ace.Ace;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class GenericArrays extends Ace {
-    
-    // COMPOSITION
-    
-    public static final <T> T[] make(final T... items) {
-        return items;
-    }
-	
-    // CONTENT
-    
-    public static final <T> boolean hasContent(final T[] array) {
-        return array != null && array.length > 0;
-    }
 
-    public static final <T> boolean nullFree(final T[] array) {
-        if (hasContent(array)) {
-            boolean result = true;
-            for (final Object o : array) {
-                result &= o != null;
-            }
-            return result;
-        }
-        return false;
-    }
+	// COMPOSITION
+	public static final <T> T[] make(final T... items) {
+		return items;
+	}
 
-    public static final <T> boolean contains(final T[] array, final T v) {
-        if (hasContent(array)) {
-            if (v == null) {
-                for (final T e : array) {
-                    if (e == null) {
-                        return true;
-                    }
-                }
-            } else {
-                for (final T e : array) {
-                    if (e == v || v.equals(e)) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-    
-    public static final <T> T get(final T[] array, final int index) {
-        return hasContent(array) && array.length > index && index >= 0 ? array[index] : null;
-    }
-    
-    public static final <T> T first(final T[] array) {
-        return hasContent(array) ? array[0] : null;
-    }
-    
-    public static final <T> T last(final T[] array) {
-        return hasContent(array) ? array[array.length - 1] : null;
-    }
-    
+	public static final <T> T[] fromList(final List<T> list) {
+		return list != null && list.size() > 0 ? list.toArray((T[]) Array.newInstance(list.get(0).getClass(), list.size())) : null;
+	}
+
+	// CONTENT
+	public static final <T> boolean hasContent(final T[] array) {
+		return array != null && array.length > 0;
+	}
+
+	public static final <T> boolean nullFree(final T[] array) {
+		if (hasContent(array)) {
+			boolean result = true;
+			for (final Object o : array) {
+				result &= o != null;
+			}
+			return result;
+		}
+		return false;
+	}
+
+	public static final <T> boolean contains(final T[] array, final T value) {
+		if (hasContent(array)) {
+			if (value == null) {
+				for (final T element : array) {
+					if (element == null) {
+						return true;
+					}
+				}
+			} else {
+				for (final T element : array) {
+					if (element == value || value.equals(element)) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	public static final <T> T get(final T[] array, final int index) {
+		return hasContent(array) && array.length > index && index >= 0 ? array[index] : null;
+	}
+
+	public static final <T> T first(final T[] array) {
+		return hasContent(array) ? array[0] : null;
+	}
+
+	public static final <T> T last(final T[] array) {
+		return hasContent(array) ? array[array.length - 1] : null;
+	}
+
+	public static final <T> T[] firstAndLast(final T[] array) {
+		return hasContent(array) ? make(array[0], array[array.length - 1]) : null;
+	}
+
+	public static <T> T[] chopBeginning(final T[] array, final int count) {
+		T[] result = null;
+		if (hasContent(array)) {
+			final ArrayList<T> list = new ArrayList(Arrays.asList(array));
+			int x = 0;
+			while (count > x++ && !list.isEmpty()) {
+				list.remove(0);
+			}
+			result = fromList(list);
+		}
+		return result;
+	}
+
+	public static <T> T[] chopEnding(final T[] array, final int count) {
+		T[] result = null;
+		if (hasContent(array)) {
+			final ArrayList<T> list = new ArrayList(Arrays.asList(array));
+			int x = 0;
+			while (count > x++ && !list.isEmpty()) {
+				list.remove(list.size() - 1);
+			}
+			result = fromList(list);
+		}
+		return result;
+	}
+
+	public static <T> T[] chopBoth(final T[] array, final int count) {
+		T[] result = null;
+		if (hasContent(array)) {
+			final ArrayList<T> list = new ArrayList(Arrays.asList(array));
+			int x = 0;
+			while (count > x++ && !list.isEmpty()) {
+				list.remove(0);
+				list.remove(list.size() - 1);
+			}
+			result = fromList(list);
+		}
+		return result;
+	}
+
+	public static final <T> T[] concat(final T[]... arrays) {
+		final ArrayList<T> list = new ArrayList();
+		for (final T[] array : arrays) {
+			list.addAll(Arrays.asList(array));
+		}
+		return fromList(list);
+	}
+
 }
--- test/ace/arrays/GenericArraysTest.java
+++ test/ace/arrays/GenericArraysTest.java
@@ -2,54 +2,112 @@
 
 package ace.arrays;
 
+import java.util.ArrayList;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class GenericArraysTest {
-    
-    @Test public void testMake() {
-        Assert.assertArrayEquals(new Object[] {}, GenericArrays.make());
-        Assert.assertArrayEquals(new Byte[] {
-            (byte) 0x01, (byte) 0x02, (byte) 0x03
-        }, GenericArrays.make((byte) 0x01, (byte) 0x02, (byte) 0x03));
-    }
-    
-    @Test public void testHasContent() {
-        Assert.assertEquals(true, GenericArrays.hasContent(GenericArrays.make(1, 2, 3)));
-        Assert.assertEquals(false, GenericArrays.hasContent(GenericArrays.make()));
-        Assert.assertEquals(false, GenericArrays.hasContent(null));
-    }
-    
-    @Test public void testNullFree() {
-        Assert.assertEquals(true, GenericArrays.nullFree(GenericArrays.make(1, 2, 3)));
-        Assert.assertEquals(false, GenericArrays.nullFree(GenericArrays.make(1, null, 3)));
-        Assert.assertEquals(false, GenericArrays.nullFree(null));
-    }
-    
-    @Test public void testContains() {
-        Assert.assertEquals(true, GenericArrays.contains(GenericArrays.make(1, 2, 3), 1));
-        Assert.assertEquals(true, GenericArrays.contains(GenericArrays.make(1, 2, 3), 2));
-        Assert.assertEquals(true, GenericArrays.contains(GenericArrays.make(1, 2, 3), 3));
-        Assert.assertEquals(false, GenericArrays.contains(GenericArrays.make(1, 2, 3), 4));
-        Assert.assertEquals(false, GenericArrays.contains(GenericArrays.make(1, 2, 3), null));
-        Assert.assertEquals(false, GenericArrays.contains(null, 4));
-    }
-    
-    @Test public void testGet() {
-        Assert.assertEquals("def", GenericArrays.get(GenericArrays.make("abc", "def"), 1));
-        Assert.assertEquals(null, GenericArrays.get(GenericArrays.make(), 0));
-    }
-    
-    @Test public void testFirst() {
-        Assert.assertEquals(Boolean.TRUE, GenericArrays.first(GenericArrays.make(true, false)));
-        final int x = GenericArrays.first(GenericArrays.make(1, 2, 3));
-        Assert.assertEquals(1, x);
-    }
-    
-    @Test public void testLast() {
-        Assert.assertEquals(Boolean.FALSE, GenericArrays.last(GenericArrays.make(true, false)));
-        final int x = GenericArrays.last(GenericArrays.make(1, 2, 3));
-        Assert.assertEquals(3, x);
-    }
-    
+
+	@Test public void testMake() {
+		Assert.assertArrayEquals(new Object[] {}, GenericArrays.make());
+		Assert.assertArrayEquals(new Byte[] {
+			(byte) 0x01, (byte) 0x02, (byte) 0x03
+		}, GenericArrays.make((byte) 0x01, (byte) 0x02, (byte) 0x03));
+	}
+
+	@Test public void testFromList() {
+		Assert.assertArrayEquals(new Integer[] { 1, 2 }, GenericArrays.fromList(new ArrayList<Integer>() {
+			{
+				add(1);
+				add(2);
+			}
+		}));
+		final Integer[] tmp = GenericArrays.fromList(new ArrayList<Integer>() {
+			{
+				add(1);
+				add(2);
+				add(3);
+			}
+		});
+		Assert.assertArrayEquals(new Integer[] { 1, 2, 3 }, tmp);
+		Assert.assertArrayEquals(null, GenericArrays.fromList(new ArrayList<Integer>()));
+		Assert.assertArrayEquals(null, GenericArrays.fromList(null));
+	}
+
+	@Test public void testHasContent() {
+		Assert.assertEquals(true, GenericArrays.hasContent(GenericArrays.make(1, 2, 3)));
+		Assert.assertEquals(false, GenericArrays.hasContent(GenericArrays.make()));
+		Assert.assertEquals(false, GenericArrays.hasContent(null));
+	}
+
+	@Test public void testNullFree() {
+		Assert.assertEquals(true, GenericArrays.nullFree(GenericArrays.make(1, 2, 3)));
+		Assert.assertEquals(false, GenericArrays.nullFree(GenericArrays.make(1, null, 3)));
+		Assert.assertEquals(false, GenericArrays.nullFree(null));
+	}
+
+	@Test public void testContains() {
+		Assert.assertEquals(true, GenericArrays.contains(GenericArrays.make(1, 2, 3), 1));
+		Assert.assertEquals(true, GenericArrays.contains(GenericArrays.make(1, 2, 3), 2));
+		Assert.assertEquals(true, GenericArrays.contains(GenericArrays.make(1, 2, 3), 3));
+		Assert.assertEquals(false, GenericArrays.contains(GenericArrays.make(1, 2, 3), 4));
+		Assert.assertEquals(false, GenericArrays.contains(GenericArrays.make(1, 2, 3), null));
+		Assert.assertEquals(false, GenericArrays.contains(null, 4));
+	}
+
+	@Test public void testGet() {
+		Assert.assertEquals("def", GenericArrays.get(GenericArrays.make("abc", "def"), 1));
+		Assert.assertEquals(null, GenericArrays.get(GenericArrays.make(), 0));
+	}
+
+	@Test public void testFirst() {
+		Assert.assertEquals(Boolean.TRUE, GenericArrays.first(GenericArrays.make(true, false)));
+		final int x = GenericArrays.first(GenericArrays.make(1, 2, 3));
+		Assert.assertEquals(1, x);
+	}
+
+	@Test public void testLast() {
+		Assert.assertEquals(Boolean.FALSE, GenericArrays.last(GenericArrays.make(true, false)));
+		final int x = GenericArrays.last(GenericArrays.make(1, 2, 3));
+		Assert.assertEquals(3, x);
+	}
+
+	@Test public void testFirstAndLast() {
+		Assert.assertArrayEquals(new Integer[] { 1, 3 }, GenericArrays.firstAndLast(GenericArrays.make(1, 2, 3)));
+		final Object a = new Object();
+		final Object b = new Object();
+		final Object c = null;
+		final Object d = null;
+		Assert.assertArrayEquals(new Object[] { a, d }, GenericArrays.firstAndLast(GenericArrays.make(a, b, c, d)));
+		Assert.assertArrayEquals(new Integer[] { 1, 1 }, GenericArrays.firstAndLast(GenericArrays.make(1)));
+	}
+
+	@Test public void testChopBeginning() {
+		Assert.assertArrayEquals(new Integer[] { 2, 3 }, GenericArrays.chopBeginning(GenericArrays.make(1, 2, 3), 1));
+		Assert.assertArrayEquals(new Integer[] { 3, 4 }, GenericArrays.chopBeginning(GenericArrays.make(1, 2, 3, 4), 2));
+		Assert.assertArrayEquals(null, GenericArrays.chopBeginning(GenericArrays.make(1, 2, 3, 4), 4));
+		Assert.assertArrayEquals(null, GenericArrays.chopBeginning(GenericArrays.make(1, 2, 3, 4), 5));
+	}
+
+	@Test public void testChopEnding() {
+		Assert.assertArrayEquals(new Integer[] { 1, 2 }, GenericArrays.chopEnding(GenericArrays.make(1, 2, 3), 1));
+		Assert.assertArrayEquals(new Integer[] { 1, 2 }, GenericArrays.chopEnding(GenericArrays.make(1, 2, 3, 4), 2));
+		Assert.assertArrayEquals(null, GenericArrays.chopEnding(GenericArrays.make(1, 2, 3, 4), 4));
+		Assert.assertArrayEquals(null, GenericArrays.chopEnding(GenericArrays.make(1, 2, 3, 4), 5));
+	}
+
+	@Test public void testChopBoth() {
+		Assert.assertArrayEquals(new Integer[] { 3, 4 }, GenericArrays.chopBoth(GenericArrays.make(1, 2, 3, 4, 5, 6), 2));
+		Assert.assertArrayEquals(new Integer[] { 2 }, GenericArrays.chopBoth(GenericArrays.make(1, 2, 3), 1));
+		Assert.assertArrayEquals(null, GenericArrays.chopBoth(GenericArrays.make(1, 2, 3, 4), 2));
+		Assert.assertArrayEquals(null, GenericArrays.chopBoth(GenericArrays.make(1, 2, 3, 4), 3));
+	}
+
+	@Test public void testConcat() {
+		final Integer[] tmp = GenericArrays.concat(GenericArrays.make(1, 2), GenericArrays.make(3, 4));
+		Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 4 }, tmp);
+		Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 4 }, GenericArrays.concat(new Integer[] { 1, 2 }, new Integer[] { 3, 4 }));
+		Assert.assertArrayEquals(null, GenericArrays.concat());
+	}
+
 }
